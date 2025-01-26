9c1c3f90685f1a37af9883d9951ebaa80be72dd4
hyunju1219 <kk020123@naver.com>
2025-01-26 19:35:59 +0900

    Refactor: orderService 로직중복 제거 및 메서드 분리

--- src/main/java/com/meongnyang/shop/service/user/OrderService.java
+++ src/main/java/com/meongnyang/shop/service/user/OrderService.java
@@ -7,10 +7,7 @@ import com.meongnyang.shop.dto.request.user.ReqPostOrderDto;
 import com.meongnyang.shop.dto.response.user.RespGetOrderListDto;
 import com.meongnyang.shop.entity.*;
 import com.meongnyang.shop.exception.RegisterException;
-import com.meongnyang.shop.repository.PaymentMapper;
-import com.meongnyang.shop.repository.ProductMapper;
-import com.meongnyang.shop.repository.StockDetailMapper;
-import com.meongnyang.shop.repository.StockMapper;
+import com.meongnyang.shop.repository.*;
 import com.meongnyang.shop.repository.user.UserOrderDetailMapper;
 import com.meongnyang.shop.repository.user.UserOrderMapper;
 import com.meongnyang.shop.security.principal.PrincipalUser;
@@ -31,15 +28,13 @@ import java.util.Map;
 public class OrderService {
 
     @Autowired
+    private PaymentService paymentService;
+    @Autowired
+    private UserStockService userStockService;
+    @Autowired
     private UserOrderMapper userOrderMapper;
     @Autowired
     private UserOrderDetailMapper userOrderDetailMapper;
-    @Autowired
-    private PaymentMapper paymentMapper;
-    @Autowired
-    private StockMapper stockMapper;
-    @Autowired
-    private StockDetailMapper stockDetailMapper;
 
     private void getCurrentUser(Long userId) {
         PrincipalUser principalUser = (PrincipalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -48,80 +43,71 @@ public class OrderService {
         }
     }
 
+    //주문을 저장하는 부분(주문 -> 기본 주문, 상세 주문, 가재고, 가재고 상세)
     @Transactional(rollbackFor = RegisterException.class)
     public void postProductsOrder(ReqPostOrderDto dto) {
        getCurrentUser(dto.getUserId());
         try {
-            Payment payment = paymentMapper.findPaymentMethodByName(dto.getPaymentMethod());
-            Order order = dto.toEntity(payment.getId());
-            //주문 추가
-            userOrderMapper.save(order);
+            Payment payment = paymentService.getPaymentMethod(dto.getPaymentMethod());
+
+            Long orderId = registerOrder(dto, payment.getId());
 
             for (ReqPostOrderDto.ProductEasy product : dto.getProducts()) {
-                //주문 상품 추가
-                OrderDetail orderDetail = OrderDetail.builder()
-                        .orderId(order.getId())
-                        .productId(product.getProductId())
-                        .productPrice(product.getProductPrice())
-                        .productCount(Long.valueOf(product.getProductCount()))
-                        .build();
-                userOrderDetailMapper.save(orderDetail);
-                //가재고 빼기, 재고 상세 추가(배송중으로)
-                Map<String, Object> params = Map.of(
-                        "productId", product.getProductId(),
-                        "productCount", product.getProductCount()
-                );
-                //가재고에서 수량만큼 빼기
-                stockMapper.modifyExpectedStockByProductId(params);
-                //재고 아이디 가져오기
-                Stock stock = stockMapper.findStockByProductId(product.getProductId());
-                //재고 상세에 배송중 상태로 추가
-                stockDetailMapper.saveOrder(StockDetail.builder()
-                                .stockId(stock.getId())
-                                .status("배송중")
-                                .arrivalQuantity(product.getProductCount())
-                                .orderDetailId(orderDetail.getId())
-                                .build());
+                Long orderDetailId = registerOrderDetail(product, orderId);
+
+                userStockService.processRegisterStock(product, orderDetailId);
             }
         } catch (Exception e) {
             throw new RegisterException(e.getMessage());
         }
     }
 
-    // 재고확정
+    //재고확정(주문 테이블의 상태를 변경하고 재고 차감)
     @Transactional(rollbackFor = RegisterException.class)
-    public void modifyProductsOrder(ReqModifyOrderDto dto) {
+    public void modifyProductsOrder(ReqModifyOrderDto dto) { //modifyOrderStatus
         getCurrentUser(dto.getUserId());
 
         try {
-            Map<String, Object> params = Map.of(
-                    "userId", dto.getUserId(),
-                    "id", dto.getId(),
-                    "orderStatus", dto.getOrderStatus()
-            );
-            // 주문 테이블의 orderStatus를 환불완료 및 구매확정으로 변경
-            userOrderMapper.modifyOrder(params);
-
+            modifyOrder(dto);
             //주문아이디로 주문상세의 상품 리스트들 가져옴(productId와 productCount필요)
-            List<OrderDetail> orderDetailList = userOrderDetailMapper.findOrderProductIdByOrderId(dto.getId());
+            List<OrderDetail> orderDetailList = getOrderDetailByOrderId(dto.getId());
 
-            for (int i = 0; i < orderDetailList.size(); i++) {
-                Map<String, Object> productDetail = Map.of(
-                        "productId", orderDetailList.get(i).getProductId(),
-                        "productCount", orderDetailList.get(i).getProductCount(),
-                        "orderStatus", dto.getOrderStatus()
-                );
-                // 환불완료시 재고 상세에 상태를 "취소"로 변경 => 가재고를 상품 개수만큼 더함
-                // 구매확정시 재고 상세에 상태를 "구매확정"으로 변경 => 현재재고에서 상품 개수만큼 뺌
-                stockDetailMapper.modifyStatusByOrderDetailId(StockDetail.builder()
-                        .orderDetailId(orderDetailList.get(i).getId())
-                        .status(dto.getOrderStatus().equals("구매확정") ? "구매확정" : "취소")
-                        .build());
-                stockMapper.modifyCurrentStockByProductId(productDetail);
-            }
+            userStockService.processModifyStock(orderDetailList, dto.getOrderStatus());
+
         } catch (Exception e) {
             throw new RegisterException(e.getMessage());
         }
+    }
+
+    private Long registerOrder(ReqPostOrderDto dto, int paymentId ) {
+        Order order = dto.toEntity(paymentId);
+        userOrderMapper.save(order);
+        return order.getId();
+    }
+
+    private Long registerOrderDetail(ReqPostOrderDto.ProductEasy product, Long orderId) {
+            OrderDetail orderDetail = OrderDetail.builder()
+                    .orderId(orderId)
+                    .productId(product.getProductId())
+                    .productPrice(product.getProductPrice())
+                    .productCount(Long.valueOf(product.getProductCount()))
+                    .build();
+            userOrderDetailMapper.save(orderDetail);
+
+            return orderDetail.getId();
+    }
+
+    private void modifyOrder(ReqModifyOrderDto dto) {
+        Map<String, Object> params = Map.of(
+                "userId", dto.getUserId(),
+                "id", dto.getId(),
+                "orderStatus", dto.getOrderStatus()
+        );
+        userOrderMapper.modifyOrder(params);
+    }
+
+    private List<OrderDetail> getOrderDetailByOrderId(Long orderId) {
+        return userOrderDetailMapper.findOrderProductIdByOrderId(orderId);
     }
 
     //주문/배송 내역 조회
@@ -168,28 +154,9 @@ public class OrderService {
         List<Order> orderList = userOrderMapper.findArrivingStatusOrderList();
         for (Order order : orderList) {
             ReqModifyOrderDto dto = new ReqModifyOrderDto(order.getId(), order.getUserId(), "구매확정");
-            Map<String, Object> params = Map.of(
-                    "userId", dto.getUserId(),
-                    "id", dto.getId(),
-                    "orderStatus", dto.getOrderStatus()
-            );
-            // 주문 테이블의 orderStatus를 환불완료 및 구매확정으로 변경
-            userOrderMapper.modifyOrder(params);
-            List<OrderDetail> orderDetailList = userOrderDetailMapper.findOrderProductIdByOrderId(dto.getId());
-            for (int i = 0; i < orderDetailList.size(); i++) {
-                Map<String, Object> productDetail = Map.of(
-                        "productId", orderDetailList.get(i).getProductId(),
-                        "productCount", orderDetailList.get(i).getProductCount(),
-                        "orderStatus", dto.getOrderStatus()
-                );
-                // 환불완료시 재고 상세에 상태를 "취소"로 변경 => 가재고를 상품 개수만큼 더함
-                // 구매확정시 재고 상세에 상태를 "구매확정"으로 변경 => 현재재고에서 상품 개수만큼 뺌
-                stockDetailMapper.modifyStatusByOrderDetailId(StockDetail.builder()
-                        .orderDetailId(orderDetailList.get(i).getId())
-                        .status(dto.getOrderStatus().equals("구매확정") ? "구매확정" : "취소")
-                        .build());
-                stockMapper.modifyCurrentStockByProductId(productDetail);
-            }
+            modifyOrder(dto);
+            List<OrderDetail> orderDetailList = getOrderDetailByOrderId(dto.getId());
+            userStockService.processModifyStock(orderDetailList, dto.getOrderStatus());
         }
     }
 }
